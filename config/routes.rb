931d4ef132f39bf8e8f2345675a5384ec7311f0c
lukesherwood <luke.sherwood55@gmail.com>
2020-04-11 11:25:27 +1200

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get '/patients/:id', to: 'patients#show'
+  get '/students', to: 'students#index'
 end
