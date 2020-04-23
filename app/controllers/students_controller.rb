b421e544c6c69aa1a57f3ba090308094e5e88a66
Ryan Mui <reachryanmui@gmail.com>
2020-04-23 02:36:39 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,11 @@
 class StudentsController < ApplicationController
     def index
-        render "index"
-        # @list = Student.all
+        @list = Student.all
     end
 end
+
+# <!-- <h1>Welcome to the Index</h1> -->
+# <!-- <% @list.each do |name| %>
+#   <p><%= name%>
+#
+# <% end %> -->
