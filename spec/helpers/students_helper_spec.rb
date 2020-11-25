d20fdc4b5712d097d9b841c8bedb4378b0f24dc1
dmepilot <dmepilot@yahoo.com>
2020-11-25 13:42:08 -0700

    Done.

--- spec/helpers/students_helper_spec.rb
+++ spec/helpers/students_helper_spec.rb
@@ -10,6 +10,11 @@ require 'rails_helper'
 #     end
 #   end
 # end
-# RSpec.describe StudentsHelper, type: :helper do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+ RSpec.describe StudentsHelper, type: :helper do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe "string concat" do
+        it "concats two strings with spaces" do
+          expect(helper.concat_strings("this","that")).to eq("this that")
+        end
+      end
+end
